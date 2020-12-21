1c5b03a29421de7be9c6605a5f1aea6c939639b2
reyes2981 <noe.reyes92@yahoo.com>
2020-12-21 15:47:46 -0600

    commit tree 6a7d33c780ab1a8f398d821c38230a26ec1df4fc

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,19 @@
 class Cat
-  # code goes here
+
+  attr_reader :name
+  attr_accessor :owner, :mood
+
+  @@all = [] #class variable 
+
+  def initialize(name, owner) #instance method
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@all << self
+  end
+
+  def self.all #class method
+    @@all 
+  end
+
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,68 @@
-class Owner
-  # code goes here
+class Owner 
+
+  attr_reader :name, :species
+  attr_accessor 
+
+  @@all = []
+  
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+  end
+
+  def say_species
+   "I am a #{@species}."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.size
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def cats
+    Cat.all.select{|cat| p cat.owner == self} #when a test wants you to return a collection you can use the .select method.
+  end
+
+  def dogs 
+    Dog.all.select{|dog| p dog.owner == self}
+  end
+
+  def buy_cat(name)
+    Cat.new(name, self)
+  end
+
+  def buy_dog(name)
+    Dog.new(name, self) # Make a new instance of the appropriate pet, initializing it with that name
+                        # and the owner who is purchasing it_.
+  end
+
+  def walk_dogs
+    self.dogs.each {|dog| dog.mood = "happy"} #walk_dogs walks the dogs which makes the dogs' moods happy
+  end
+
+  def feed_cats
+    self.cats.each {|cat| cat.mood = "happy"}
+  end
+
+  def sell_pets
+    pets = self.dogs + self.cats #passing classes as values 
+      pets.each do |pet|
+        pet.mood = "nervous"
+        pet.owner = nil
+      end
+  end
+
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end
+  
 end
